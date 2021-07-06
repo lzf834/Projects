35bc6419fe4a77f568a30c4bad5cbd13e51a5afc
ZF <zifoongl@gmail.com>
2021-07-06 23:16:30 +0800

    Hello World became a BMI calculator

--- welcome dotnet/HelloWorld/Program.cs
+++ welcome dotnet/HelloWorld/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -6,7 +10,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to BMI Calc! Please input your height in cm.");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now please input your weight in kg.");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            var currentDate = DateTime.Now;
+            double BMI = Math.Round(weight * weight / height, 2);
+            Console.WriteLine($"{Environment.NewLine}Hello, on {currentDate:d} at {currentDate:t}, your BMI is {BMI}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
